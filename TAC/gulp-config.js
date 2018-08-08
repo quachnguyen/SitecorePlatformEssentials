f3ec8003bf2ee3d6509ff27a7777bd3afebae3dd
Quách Nguyên <quachngochoangnguyen@gmail.com>
2018-08-09 00:16:03 +0700

    Added Gulp files

---
+++ TAC/gulp-config.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+    var config = {
+        webRoot: "C:\\inetpub\\wwwroot\\sc92.local",
+        devRoot: "C:\\Projects\\GitHub\\SitecorePlatformEssentials\\TAC",
+        solutionName: "TAC",
+        buildConfiguration: "Debug"
+    };
+    return config;
+}
---
+++ TAC/gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require("gulp");
+var msbuild = require("gulp-msbuild");
+var debug = require("gulp-debug");
+var foreach = require("gulp-foreach");
+var gulpConfig = require("./gulp-config.js")();
+module.exports.config = gulpConfig;
+gulp.task("Publish-Site", function () {
+    return gulp.src("./*events.tac.local*/*.csproj")
+        .pipe(foreach(function (stream, file) {
+            return stream
+                .pipe(debug({ title: "Publishing " }))
+                .pipe(msbuild({
+                    targets: ["Build"],
+                    gulpConfiguration: gulpConfig.buildConfiguration,
+                    properties: {
+                        publishUrl: gulpConfig.webRoot,
+                        DeployDefaultTarget: "WebPublish",
+                        WebPublishMethod: "FileSystem",
+                        DeployOnBuild: "true",
+                        DeleteExistingFiles: "false",
+                        _FindDependencies: "false"
+                    }
+                }));
+        }));
+});
